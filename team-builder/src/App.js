8c3de56b63b6a27b0f73f92f609aabbba33dc594
Suparada Saitalae <ann.suparada@gmail.com>
2019-07-24 15:23:23 -0500

    import useState, create Form.js

---
+++ team-builder/src/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import Form from "./Form"
+
+import './App.css';
+
+
+
+function App() {
+  const [team, setTeam] = useState([]);
+
+
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
